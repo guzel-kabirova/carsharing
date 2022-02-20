35f2708346182173d1466f22756fcaa3f1ff2d66
guzel-kabirova <vkcom1998@gmail.com>
2022-02-20 19:48:11 +0300

    F-01 feat: Make slider logic

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,32 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
+import {Slide} from './landing-page.interface';
+
+const SLIDES: Slide[] = [
+  {
+    picture: {src: '../../../assets/pictures/parking.jpg', alt: 'parking'},
+    title: 'Бесплатная парковка',
+    text: 'Оставляйте машину на платных городских парковках и разрешенных местах, не нарушая ПДД, а также в аэропортах.',
+    btnColor: 'linear-gradient(to right, #13493F, #0C7B1B)'
+  },
+  {
+    picture: {src: '../../../assets/pictures/insurance.jpg', alt: 'insurance'},
+    title: 'Страховка',
+    text: 'Полная страховка автомобиля',
+    btnColor: 'linear-gradient(to right, #132949, #0C7B67)'
+  },{
+    picture: {src: '../../../assets/pictures/petrol.jpg', alt: 'petrol'},
+    title: 'Бензин',
+    text: 'Полный бак на любой заправке города за наш счёт',
+    btnColor: 'linear-gradient(to right, #493013, #7B0C3B)'
+  },{
+    picture: {src: '../../../assets/pictures/service.jpg', alt: 'service'},
+    title: 'Обслуживание',
+    text: 'Автомобиль проходит еженедельное ТО',
+    btnColor: 'linear-gradient(to right, #281349, #720C7B)'
+  },
+]
+
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
@@ -7,6 +34,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LandingPageComponent implements OnInit {
+  slides: Slide[] = SLIDES;
 
   constructor() { }
 
--- src/app/shared/components/slider/slider.component.ts
+++ src/app/shared/components/slider/slider.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {Component, OnInit, ChangeDetectionStrategy, Input} from '@angular/core';
+import {Slide} from '../../../landing-page/landing-page.interface';
 
 @Component({
   selector: 'app-slider',
@@ -7,10 +8,34 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SliderComponent implements OnInit {
+  @Input()
+  slides?: Slide[];
+
+  public currentSlide = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public increaseCurrentSlide() {
+    if (this.slides) {
+      if (this.currentSlide === this.slides.length - 1) {
+        this.currentSlide = 0;
+        return;
+      }
+      this.currentSlide ++;
+    }
+  }
+
+  public decreaseCurrentSlide() {
+    if (this.slides) {
+      if (this.currentSlide === 0) {
+        this.currentSlide = this.slides.length - 1;
+        return;
+      }
+      this.currentSlide --;
+    }
   }
 
 }
