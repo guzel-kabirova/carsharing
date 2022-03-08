1e589ba802189330e58e196f0bceee73bc4cba07
guzel-kabirova <vkcom1998@gmail.com>
2022-03-08 16:23:42 +0300

    F-04 refactor

--- src/app/order-page/steps/step-model/step-model.component.ts
+++ src/app/order-page/steps/step-model/step-model.component.ts
@@ -1,5 +1,5 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 import {StepModelFacadeService} from './services/step-model.facade.service';
 import {ALL_CATEGORIES} from './step-model.const';
@@ -12,7 +12,6 @@ import {CarModel} from './step-model.interface';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StepModelComponent implements OnInit {
-  public formFilter?: FormGroup;
   public form?: FormGroup;
   public cars$ = this._facade.store.cars$;
 
@@ -25,20 +24,14 @@ export class StepModelComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.formFilter = this._fb.group({
+    this.form = this._fb.group({
       model: [''],
     });
 
-    this.form = this._fb.group({
-      car: ['', Validators.required],
-    });
   }
 
   public selectCar(car: CarModel) {
     this._facade.changeActiveCar(car.id);
     this._facade.changeCarModel(car);
-    this.form?.setValue({
-      car: car.name,
-    });
   }
 }
